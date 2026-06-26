3343269e331d0b85c854dc9bdc9b72d34c61bf12
Aditya Gupta <guptaaditya0530@gmail.com>
2026-06-26 11:47:47 +0530

    env process

--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 require("dotenv").config();
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-const LOG_API_URL = "YOUR_LOG_API_URL";
+const LOG_API_URL = "http://4.224.186.213/evaluation-service/logs";
 
 async function Log(stack, level, packageName, message) {
     try {
