9299dfa0273c1877183fac5a483ebb69ce4a705a
Aditya Gupta <guptaaditya0530@gmail.com>
2026-06-26 12:10:17 +0530

    next

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
+
+
 const Log = require("./Log");
 
-Log("backend", "error", "auth", "Login failed test");
+async function test() {
+    await Log("backend", "info", "auth", "Login API hit");
+    await Log("backend", "error", "auth", "Login failed test");
+}
+
+test();
