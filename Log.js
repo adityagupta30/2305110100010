029a2534399b57d6cead0f0b07531accfb8a1206
Aditya Gupta <guptaaditya0530@gmail.com>
2026-06-26 12:47:10 +0530

    next step done

--- Log.js
+++ Log.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const LOG_API_URL = "http://4.224.186.213/evaluation-service/logs";
 
+// Debug
+console.log("ACCESS_TOKEN:", ACCESS_TOKEN);
+
 async function Log(stack, level, packageName, message) {
     try {
         const payload = {
@@ -20,13 +23,20 @@ async function Log(stack, level, packageName, message) {
             }
         };
 
+        // Debug
+        console.log("Headers:", config.headers);
+        console.log("Payload:", payload);
+
         const res = await axios.post(LOG_API_URL, payload, config);
+
+        console.log("API Success:", res.data);
 
         return res.data;
     } catch (err) {
         console.log("Logging Failed");
 
         if (err.response) {
+            console.log("Status:", err.response.status);
             console.log("Error Response:", err.response.data);
         } else {
             console.log("Error:", err.message);
